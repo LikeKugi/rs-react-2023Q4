94201185281647a220903ea341ff520b2f6da8eb
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-10-23 23:48:51 +0300

    feat: add eslint and prettier to the project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,22 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "prettier",
+    "plugin:prettier/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [

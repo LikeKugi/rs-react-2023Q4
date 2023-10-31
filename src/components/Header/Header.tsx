5d2c6e2a7aad5f45b3b529f9e75ca6a057784eff
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-10-31 22:59:52 +0300

    feat: change Header to functional component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,21 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import styles from './Header.module.scss';
 
 interface IHeaderProps {
   handleThrow: () => void;
 }
 
-class Header extends Component<IHeaderProps> {
-  constructor(props: IHeaderProps) {
-    super(props);
-  }
-  public render() {
-    return (
-      <header className={styles.header}>
-        <div className={styles.header__container}>
-          <h3>Rs School React App</h3>
-          <button
-            className={styles.header__btn}
-            onClick={this.props.handleThrow}
-          >
-            Throw Error
-          </button>
-        </div>
-      </header>
-    );
-  }
-}
+const Header: FC<IHeaderProps> = ({ handleThrow }) => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__container}>
+        <h3>Rs School React App</h3>
+        <button className={styles.header__btn} onClick={handleThrow}>
+          Throw Error
+        </button>
+      </div>
+    </header>
+  );
+};
+
 export default Header;

fa4df916f41552cedc86e48ba2c8f691e2fffadc
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-10-31 22:58:47 +0300

    feat: change Loader to functional component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,17 +1,15 @@
-import { Component } from 'react';
 import styles from './Loader.module.scss';
 
-class Loader extends Component {
-  public render() {
-    return (
-      <div className={styles.back}>
-        <div className={styles.loader}>
-          {[...Array(4)].map((_, i) => (
-            <div key={i} className={styles.div} />
-          ))}
-        </div>
+const Loader = () => {
+  return (
+    <div className={styles.back}>
+      <div className={styles.loader}>
+        {[...Array(4)].map((_, i) => (
+          <div key={i} className={styles.div} />
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default Loader;

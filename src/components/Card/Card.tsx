918707acd91635514998ddc6a268feb1b454ec2f
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-10-31 22:55:07 +0300

    feat: change card to functional component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import { IArtwork } from '@/types/api/IArtwork';
 import styles from './Card.module.scss';
 
@@ -6,29 +6,23 @@ interface ICardProps {
   content: IArtwork;
 }
 
-class Card extends Component<ICardProps, object> {
-  constructor(props: ICardProps) {
-    super(props);
-  }
-
-  public render() {
-    return (
-      <div className={styles.card}>
-        <img
-          className={styles.card__img}
-          src={`https://www.artic.edu/iiif/2/${this.props.content.image_id}/full/200,/0/default.jpg`}
-          alt={this.props.content.title}
-        />
-        <div className={styles.card__body}>
-          <p className={styles.card__title}>{this.props.content.title}</p>
-          <p>{this.props.content.artist_title}</p>
-          <p>
-            {this.props.content.date_start} - {this.props.content.date_end}
-          </p>
-        </div>
+const Card: FC<ICardProps> = ({ content }) => {
+  return (
+    <div className={styles.card}>
+      <img
+        className={styles.card__img}
+        src={`https://www.artic.edu/iiif/2/${content.image_id}/full/200,/0/default.jpg`}
+        alt={content.title}
+      />
+      <div className={styles.card__body}>
+        <p className={styles.card__title}>{content.title}</p>
+        <p>{content.artist_title}</p>
+        <p>
+          {content.date_start} - {content.date_end}
+        </p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Card;

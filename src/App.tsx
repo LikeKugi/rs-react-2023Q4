270e2fcd97403019f84c4292aee78168d994ee42
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-10-31 22:53:07 +0300

    feat: change app to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useCallback, useEffect, useReducer, useState } from 'react';
 import {
   getDataFromStorage,
   setDataToLocalStorage,
@@ -14,136 +14,87 @@ import styles from './App.module.scss';
 import Loader from '@/components/Loader/Loader';
 import { IQueryObject } from '@/types/appTypes';
 
-interface IAppState {
-  toThrow: boolean;
-  query: string;
-  limit: number;
-  page: number;
-  artworks: IArtwork[];
-  loading: boolean;
-}
+const App = () => {
+  const [errorState, errorDispatch] = useReducer(
+    (prevState) => !prevState,
+    false,
+  );
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState('');
+  const [limit, setLimit] = useState(0);
+  const [page, setPage] = useState(0);
+  const [artworks, setArtworks] = useState<IArtwork[]>([]);
 
-class App extends Component<object, IAppState> {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      toThrow: false,
-      query: '',
-      limit: 8,
-      page: 1,
-      artworks: [],
-      loading: true,
-    };
-    this.throwError = this.throwError.bind(this);
-    this.handleThrow = this.handleThrow.bind(this);
-    this.changeQuery = this.changeQuery.bind(this);
-    this.handleSubmitForm = this.handleSubmitForm.bind(this);
-    this.placeArtWorksData = this.placeArtWorksData.bind(this);
-    this.fetchArtworks = this.fetchArtworks.bind(this);
-  }
+  const fetchArtworks = useCallback(
+    (query: string) => {
+      const initialParamsObj: IFetchQueryParams = {
+        limit: limit.toString(),
+        page: page.toString(),
+        fields: 'id,title,image_id,artist_title,date_start,date_end',
+      };
+      if (query) {
+        initialParamsObj.q = query;
+      }
+      const params = new URLSearchParams(initialParamsObj);
+      const basePath: string = query
+        ? `${ApiConstants.BASE}${ApiConstants.ARTWORKS}/${ApiConstants.SEARCH}?${params}`
+        : `${ApiConstants.BASE}${ApiConstants.ARTWORKS}?${params}`;
+      (fetchData(basePath) as Promise<IBaseTypeResponse>)
+        .then((a) => {
+          setArtworks(a.data);
+        })
+        .catch((e) => console.log(e))
+        .finally(() => {
+          setLoading(false);
+        });
+    },
+    [limit, page],
+  );
 
-  public componentDidUpdate() {
-    if (this.state.toThrow) {
-      this.throwError();
+  useEffect(() => {
+    if (errorState) {
+      throw new Error('Error was thrown buy user');
     }
-  }
+  }, [errorState]);
 
-  public componentDidMount() {
+  useEffect(() => {
     const fallback: IQueryObject = {
       query: '',
     };
     const queryObject = getDataFromStorage(fallback) as IQueryObject;
-    let query = this.state.query;
-    if (queryObject.query !== query) {
-      query = queryObject.query;
-      this.setState((prev) => ({
-        ...prev,
-        loading: false,
-        query,
-      }));
-    }
-    this.fetchArtworks(query);
-  }
+    setQuery(queryObject.query);
+    setLimit(8);
+    setPage(1);
+    setLoading(true);
+  }, []);
 
-  fetchArtworks(query: string) {
-    const initialParamsObj: IFetchQueryParams = {
-      limit: this.state.limit.toString(),
-      page: this.state.page.toString(),
-      fields: 'id,title,image_id,artist_title,date_start,date_end',
-    };
-    if (query) {
-      initialParamsObj.q = query;
-    }
-    const params = new URLSearchParams(initialParamsObj);
-    const basePath: string = query
-      ? `${ApiConstants.BASE}${ApiConstants.ARTWORKS}/${ApiConstants.SEARCH}?${params}`
-      : `${ApiConstants.BASE}${ApiConstants.ARTWORKS}?${params}`;
-    (fetchData(basePath) as Promise<IBaseTypeResponse>)
-      .then((a) => {
-        this.placeArtWorksData(a.data);
-      })
-      .catch((e) => console.log(e))
-      .finally(() => {
-        this.setState((prev) => ({
-          ...prev,
-          loading: false,
-        }));
-      });
-  }
+  useEffect(() => {
+    if (!loading) return;
+    fetchArtworks(query);
+  }, [fetchArtworks, loading, query]);
 
-  placeArtWorksData(data: IArtwork[]) {
-    this.setState((prevState) => ({
-      ...prevState,
-      artworks: data,
-    }));
-  }
-
-  throwError() {
-    throw new Error('Error was thrown buy user');
-  }
-
-  handleThrow() {
-    this.setState({ toThrow: true });
-  }
-
-  changeQuery(arg: string) {
-    this.setState((prev) => ({
-      ...prev,
-      query: arg,
-    }));
-  }
-
-  handleSubmitForm(query: string) {
-    this.setState((prev) => ({
-      ...prev,
-      loading: true,
-    }));
+  const handleSubmitForm = (query: string) => {
+    setLoading(true);
     setDataToLocalStorage({ query });
-    this.fetchArtworks(query);
-  }
+  };
 
-  public render() {
-    return (
-      <div className={styles.content}>
-        <Header handleThrow={this.handleThrow} />
-        <div className={styles.content__base}>
-          <Form
-            query={this.state.query}
-            handleChangeQuery={this.changeQuery}
-            handleSubmit={this.handleSubmitForm}
-          />
-
-          {this.state.loading && <Loader />}
-          {!this.state.artworks.length && !this.state.loading && (
-            <h1 className={styles.content__alert}>No artworks found here</h1>
-          )}
-          {!!this.state.artworks.length && (
-            <CardList cards={this.state.artworks} />
-          )}
-        </div>
+  return (
+    <div className={styles.content}>
+      <Header handleThrow={errorDispatch} />
+      <div className={styles.content__base}>
+        <Form
+          query={query}
+          handleChangeQuery={setQuery}
+          handleSubmit={handleSubmitForm}
+        />
+        {loading && <Loader />}
+        {!artworks.length && !loading && (
+          <h1 className={styles.content__alert}>No artworks found here</h1>
+        )}
+        {!!artworks.length && <CardList cards={artworks} />}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;

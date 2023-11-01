1d7d9956ee9d768be957ccce2e039e37e502f663
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-11-01 23:18:09 +0300

    feat: add link in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react';
 import styles from './Header.module.scss';
+import { Link } from 'react-router-dom';
+import { RouterConstants } from '@/routes/RouterConstants';
 
 const Header: FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header__container}>
-        <h3>Rs School React App</h3>
+        <Link className={styles.header__link} to={RouterConstants.INDEX}>
+          <h3>Rs School React App</h3>
+        </Link>
       </div>
     </header>
   );

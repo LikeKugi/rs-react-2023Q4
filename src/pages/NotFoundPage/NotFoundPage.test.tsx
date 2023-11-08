6aceab0a75a80ca6938abdf0a8c705e053e07212
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-11-08 20:50:26 +0300

    test: add test for not found page to see if page changes

--- src/pages/NotFoundPage/NotFoundPage.test.tsx
+++ src/pages/NotFoundPage/NotFoundPage.test.tsx
@@ -1,8 +1,9 @@
 import { afterEach, describe, expect } from 'vitest';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import NavigationProvider from '@/provider/NavigationProvider/NavigationProvider';
 import { BrowserRouter } from 'react-router-dom';
 import NotFoundPage from '@/pages/NotFoundPage/NotFoundPage';
+import { useNavigationProvider } from '@/provider/NavigationProvider/NavigationProvider.hooks';
 
 describe('<NotFoundPage /> tests', () => {
   afterEach(() => {
@@ -46,5 +47,28 @@ describe('<NotFoundPage /> tests', () => {
     const linkElement = screen.getByRole('link');
     linkElement.focus();
     expect(linkElement).toHaveFocus();
+  });
+
+  it('should change page', function () {
+    const MockPage = () => {
+      const { page } = useNavigationProvider();
+      return <div>page: {page}</div>;
+    };
+
+    render(
+      <NavigationProvider>
+        <BrowserRouter>
+          <MockPage />
+          <NotFoundPage />
+        </BrowserRouter>
+      </NavigationProvider>,
+    );
+
+    const pageEl = screen.queryByText(/page:/i);
+    expect(pageEl).toBeInTheDocument();
+    const linkElement = screen.getByRole('link');
+
+    fireEvent.click(linkElement);
+    expect(pageEl!.textContent).toBe('page: 1');
   });
 });

cc7a886016d4407e0377338be0fbba6b1901cca5
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-11-10 12:37:09 +0300

    test: fix test for NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.test.tsx
+++ src/pages/NotFoundPage/NotFoundPage.test.tsx
@@ -1,22 +1,21 @@
-import { afterEach, describe, expect } from 'vitest';
+import { afterEach, describe, expect, vi } from 'vitest';
 import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import NotFoundPage from '@/pages/NotFoundPage/NotFoundPage';
-import { useNavigationProvider } from '@/provider/NavigationProvider/NavigationProvider.hooks';
-import { MockNavigationPageProvider } from '~/__tests__/provider/MockNavigationPageProvider/MockNavigationPageProvider';
+import * as useNavigationHook from '@/provider/NavigationProvider/NavigationProvider.hooks';
 
 describe('<NotFoundPage /> tests', () => {
+  const useSpyNavigation = vi.spyOn(useNavigationHook, 'useNavigationProvider');
   afterEach(() => {
+    useSpyNavigation.mockClear();
     cleanup();
   });
 
   it('should render component', function () {
     render(
-      <MockNavigationPageProvider>
-        <BrowserRouter>
-          <NotFoundPage />
-        </BrowserRouter>
-      </MockNavigationPageProvider>,
+      <BrowserRouter>
+        <NotFoundPage />
+      </BrowserRouter>,
     );
 
     expect(true).toBeTruthy();
@@ -24,11 +23,9 @@ describe('<NotFoundPage /> tests', () => {
 
   it('should not be empty', function () {
     render(
-      <MockNavigationPageProvider>
-        <BrowserRouter>
-          <NotFoundPage />
-        </BrowserRouter>
-      </MockNavigationPageProvider>,
+      <BrowserRouter>
+        <NotFoundPage />
+      </BrowserRouter>,
     );
 
     const headerElement = screen.queryByText('The information not found');
@@ -37,59 +34,57 @@ describe('<NotFoundPage /> tests', () => {
 
   it('should contain link', function () {
     render(
-      <MockNavigationPageProvider>
-        <BrowserRouter>
-          <NotFoundPage />
-        </BrowserRouter>
-      </MockNavigationPageProvider>,
+      <BrowserRouter>
+        <NotFoundPage />
+      </BrowserRouter>,
     );
 
     const linkElement = screen.getByRole('link');
     linkElement.focus();
     expect(linkElement).toHaveFocus();
   });
-  it('should change page', function () {
-    const MockPage = () => {
-      const { page } = useNavigationProvider();
-      return <div>page: {page}</div>;
-    };
+  it('should change page', async function () {
+    const mockHandleChangePage = vi.fn();
+    useSpyNavigation.mockReturnValue({
+      query: '',
+      setQuery: vi.fn(),
+      page: 5,
+      setPage: mockHandleChangePage,
+      limit: 12,
+      setLimit: vi.fn(),
+    });
 
     render(
-      <MockNavigationPageProvider>
-        <BrowserRouter>
-          <MockPage />
-          <NotFoundPage />
-        </BrowserRouter>
-      </MockNavigationPageProvider>,
+      <BrowserRouter>
+        <NotFoundPage />
+      </BrowserRouter>,
     );
 
-    const pageEl = screen.queryByText(/page:/i);
-    expect(pageEl).toBeInTheDocument();
     const linkElement = screen.getByRole('link');
+    expect(linkElement).toBeInTheDocument();
 
     fireEvent.click(linkElement);
-    expect(pageEl!.textContent).toBe('page: 1');
+    expect(mockHandleChangePage).toHaveBeenCalledOnce();
+    expect(mockHandleChangePage).toBeCalledWith(1);
   });
 
   it('should render NotFoundPage when route is invalid', async function () {
     render(
-      <MockNavigationPageProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path={'/'}
-              element={
-                <>
-                  <Link to={'/dfe'}>Error</Link>
-                  <Link to={'/test'}>Test</Link>
-                </>
-              }
-            />
-            <Route path={'/test'} element={<>Test</>} />
-            <Route path={'*'} element={<NotFoundPage />} />
-          </Routes>
-        </BrowserRouter>
-      </MockNavigationPageProvider>,
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={'/'}
+            element={
+              <>
+                <Link to={'/dfe'}>Error</Link>
+                <Link to={'/test'}>Test</Link>
+              </>
+            }
+          />
+          <Route path={'/test'} element={<>Test</>} />
+          <Route path={'*'} element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>,
     );
 
     expect(screen.getByText(/test/i)).toBeInTheDocument();

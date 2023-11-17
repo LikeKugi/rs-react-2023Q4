34f95e727146a36a812493511d4b3db334cad279
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-11-17 15:33:02 +0300

    test: HomePage test

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -1,43 +1,24 @@
 import { beforeEach, describe, expect, vi } from 'vitest';
 import createFetchMock from 'vitest-fetch-mock';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '@/pages/HomePage/HomePage';
-import * as useNavigationHook from '@/provider/NavigationProvider/NavigationProvider.hooks';
-import * as useArtworksHook from '@/provider/ArtworksProvider/ArtworksProvider.hooks';
+import { renderWithProviders } from '@/__tests__/utils';
 
 describe('<HomePage /> tests', () => {
-  const useSpyNavigation = vi.spyOn(useNavigationHook, 'useNavigationProvider');
-  const useSpyArtworks = vi.spyOn(useArtworksHook, 'useArtworksProvider');
   const fetchArtworks = createFetchMock(vi);
   fetchArtworks.enableMocks();
 
   beforeEach(() => {
-    useSpyNavigation.mockClear();
-    useSpyArtworks.mockClear();
     fetchArtworks.doMock();
   });
 
-  it('should render layout', async function () {
-    useSpyNavigation.mockReturnValue({
-      query: '',
-      setQuery: vi.fn(),
-      page: 1,
-      setPage: vi.fn(),
-      limit: 12,
-      setLimit: vi.fn(),
-    });
-    useSpyArtworks.mockReturnValue({
-      totalPages: 50,
-      artworks: [],
-      setArtworks: vi.fn(),
-      setTotalPages: vi.fn(),
-    });
+  it('should render empty layout when no artworks found', async function () {
     fetchArtworks.mockResponseOnce(
       () =>
         new Promise((_, reject) => setTimeout(() => reject('No data'), 100)),
     );
-    render(
+    renderWithProviders(
       <BrowserRouter>
         <Routes>
           <Route path={'/'} element={<HomePage />} />

0408923b5390a4082ca6af09848243a4b0767cef
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-11-17 14:39:52 +0300

    test: fix CardList test

--- src/components/CardList/CardList.test.tsx
+++ src/components/CardList/CardList.test.tsx
@@ -1,32 +1,30 @@
-import { afterEach, describe, expect, vi } from 'vitest';
-import * as useArtworksHook from '@/provider/ArtworksProvider/ArtworksProvider.hooks';
+import { afterEach, describe, expect } from 'vitest';
 import { datalist } from '@/components/CardList/CardList.test-mocks';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import CardList from '@/components/CardList/CardList';
+import { renderWithProviders } from '@/__tests__/utils';
 
 describe('<CardList /> component tests', () => {
-  const useSpyArtworks = vi.spyOn(useArtworksHook, 'useArtworksProvider');
-
   afterEach(() => {
     cleanup();
-    useSpyArtworks.mockClear();
   });
 
   it.each([5, 10, 15])('should render correct number of cards', function (i) {
-    useSpyArtworks.mockReturnValue({
-      totalPages: 50,
-      artworks: Array.from({ length: i }, (_, idx) => ({
-        ...datalist[0],
-        id: idx,
-      })),
-      setArtworks: vi.fn(),
-      setTotalPages: vi.fn(),
-    });
-    render(
+    renderWithProviders(
       <BrowserRouter>
         <CardList />
       </BrowserRouter>,
+      {
+        preloadedState: {
+          artworks: {
+            artworks: Array.from({ length: i }, (_, idx) => ({
+              ...datalist[0],
+              id: idx,
+            })),
+          },
+        },
+      },
     );
     expect(screen.getAllByRole('link').length).toBe(i);
   });

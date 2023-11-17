b62596bdc249ba1dd994f012ef06ca6d57e7f8ff
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-11-17 15:49:49 +0300

    test: test limit selections and pagination

--- src/components/LimitSelection/LimitSelection.test.tsx
+++ src/components/LimitSelection/LimitSelection.test.tsx
@@ -1,9 +1,13 @@
-import { afterEach, describe, expect } from 'vitest';
+import { afterEach, describe, expect, vi } from 'vitest';
 import { cleanup, fireEvent, screen } from '@testing-library/react';
 import LimitSelection from '@/components/LimitSelection/LimitSelection';
 import { renderWithProviders } from '@/__tests__/utils';
+import * as storeHooks from '@/store/hooks';
+import { setLimit } from '@/store/navigationSlice/navigationSlice';
 
 describe('<LimitSelection /> component tests', () => {
+  const spyAppDispatch = vi.spyOn(storeHooks, 'useAppDispatch');
+
   afterEach(() => {
     cleanup();
   });
@@ -14,9 +18,12 @@ describe('<LimitSelection /> component tests', () => {
   });
 
   it('should work select', function () {
+    const mockedSetterLimit = vi.fn();
+    spyAppDispatch.mockReturnValue(mockedSetterLimit);
     renderWithProviders(<LimitSelection />);
     const options = screen.getAllByRole('option');
     expect(options).toHaveLength(5);
-    fireEvent.change(screen.getByRole('combobox'), { target: { value: 8 } });
+    fireEvent.change(screen.getByRole('combobox'), { target: { value: 16 } });
+    expect(mockedSetterLimit).toBeCalledWith(setLimit(16));
   });
 });
